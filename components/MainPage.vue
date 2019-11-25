ec229db893a9633ff0fddd02dd3754a4c2409e9b
begueradj <begueradj.bill@gmail.com>
2019-11-25 08:39:55 +0200

    Add simple demo

---
+++ components/MainPage.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="hello">
+    <v-layout row>
+      <v-flex d-flex xs3>
+        <v-card tile>
+          <v-card-text>
+    
+            <v-list>
+              <draggable 
+                v-model="relsFiltered" 
+                element="div"
+                @start="drag = true" 
+                @end="drag = false">
+                <div 
+                  v-for="(el, index) in relsFiltered"
+                  @click="onSelect(el)"
+                  :key="el.id"
+                >
+                  <v-divider v-if="index === 0"/>
+
+                  <v-list-tile 
+                    :key="el.id" 
+                    ripple
+                    
+                    >
+                    <v-list-tile-content>
+                      <v-list-tile-title>
+                        {{index}}. {{el.name}}
+                      </v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+
+                  <v-divider/>
+                </div>
+              </draggable>
+            </v-list>
+    
+          </v-card-text>
+
+          <v-card-text>
+          
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex d-flex xs9>
+        <v-card tile>
+          <div>{{ relsFiltered.length }}</div>
+          <pre style="text-align: left;">{{ relsFiltered }}</pre>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    
+    </div>
+</template>
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      rels: [
+        { id: 0, name: "one" },
+        { id: 1, name: "two" },
+        { id: 2, name: "three" },
+        { id: 3, name: "four" }
+      ]
+    };
+  },
+  computed: {
+    relsFiltered: {
+      get() {
+        return this.rels;
+      },
+      set(data) {
+        this.rels = data;
+      }
+    }
+  },
+  components: { draggable },
+  methods: {
+    onSelect() {}
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+ 
